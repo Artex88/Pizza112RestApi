6672585edbf4eb3d454331566dec17d5ccce9110
Artex88 <zadevalov.alex@gmail.com>
2023-11-19 17:12:02 +0500

    inital commit

---
+++ src/main/java/ru/urfu/pizzaSite/RestApiPizzaApplication/RestApiPizzaApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.pizzaSite.RestApiPizzaApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/urfu/pizzaSite/RestApiPizzaApplication/RestApiPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.pizzaSite.RestApiPizzaApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
