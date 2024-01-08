c905f3f39f77b717fa5d8e67fc609c3a7acf2a7a
Artex88 <zadevalov.alex@gmail.com>
2024-01-08 16:08:23 +0500

    start add command to tg bot

--- src/main/java/ru/urfu/pizzaSite/RestApiPizzaApplication/config/TelegramBotConfiguration.java
+++ src/main/java/ru/urfu/pizzaSite/RestApiPizzaApplication/config/TelegramBotConfiguration.java
@@ -2,6 +2,7 @@ package ru.urfu.pizzaSite.RestApiPizzaApplication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -15,5 +16,10 @@ public class TelegramBotConfiguration {
         var api = new TelegramBotsApi(DefaultBotSession.class);
         api.registerBot(telegramBot);
         return api;
+    }
+
+    @Bean
+    public OkHttp3ClientHttpRequestFactory okHttp3Client(){
+        return new OkHttp3ClientHttpRequestFactory();
     }
 }
--- src/main/java/ru/urfu/pizzaSite/RestApiPizzaApplication/model/TelegramBot.java
+++ src/main/java/ru/urfu/pizzaSite/RestApiPizzaApplication/model/TelegramBot.java
@@ -3,15 +3,22 @@ package ru.urfu.pizzaSite.RestApiPizzaApplication.model;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-import java.util.List;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
     @Value("${telegram_bot_name}")
     private String botName;
+
+    private static final String START = "/start";
+    private static final String HELP = "/help";
+
+    private static final String CONNECT = "/connect";
+
+    private static final String DISCONNECT = "/disconnect";
 
     public TelegramBot(@Value("${telegram_bot_token}") String token){
         super(token);
@@ -23,21 +30,27 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
     @Override
     public void onUpdateReceived(Update update) {
+        if (!update.hasMessage() || !update.getMessage().hasText()){
+            return;
+        }
+        var message = update.getMessage().getText();
+        var chatId = update.getMessage().getChatId();
+        switch (message){
+            case START -> {
 
+            }
+        }
     }
 
-    @Override
-    public void onUpdatesReceived(List<Update> updates) {
-        super.onUpdatesReceived(updates);
+
+    private void sendMessage(Long chatId, String text){
+        var chatIdStr = String.valueOf(chatId);
+        var sendMessage = new SendMessage(chatIdStr, text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException ignored){
+
+        }
     }
 
-
-
-
-
-
-    @Override
-    public void onRegister() {
-        super.onRegister();
-    }
 }
